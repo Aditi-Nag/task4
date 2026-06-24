066595630335fb48cf3f451da8a3591a31ed3446
Aditi Nag <joynag151@gmail.com>
2026-06-24 23:10:38 +0600

    fixed route syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-  import { BrowserRouter, Routes, Route } from "react-router-dom";
-  import Login from "./components/Login";
-  import Register from "./components/Register";
-  import Dashboard from "./components/Dashboard";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
-  function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-        
-      </BrowserRouter>
-      
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-  export default App;
+export default App;
 
